75982e32e4969ab0663acba8618b2010598541af
folse <folsee@gmail.com>
2014-08-08 09:45:08 +0800

    Add base_site.html

--- Management/admin.py
+++ Management/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-
 from Management import models
+from django.http import HttpResponseRedirect
+import urllib
+
+
+def export_selected_objects(modeladmin, request, queryset):
+	return HttpResponseRedirect("/path/")
+
+class Place(models.Place):
+	def save_related(self, request, obj, form, change):
+		return HttpResponseRedirect("/path/")
 
 admin.site.register(models.Place)
+
+admin.site.add_action(export_selected_objects)
