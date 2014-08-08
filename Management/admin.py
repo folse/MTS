98acce34073d6dcc3688fa30dcc187bad4817e1b
folse <folsee@gmail.com>
2014-08-08 23:59:30 +0800

    Add Parse

--- Management/admin.py
+++ Management/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 from Management import models
-import urllib
+from parse_rest.connection import register
+from parse_rest.datatypes import Object
 
+
+class Place(Object):
+	pass
 
 class PlaceAdmin(admin.ModelAdmin):
 
 	def save_model(self, request, obj, form, change):
-		urllib.urlopen('http://baidu.com').read()
-
+		register('MQRrReTdb9c82PETy0BfUoL0ck6xGpwaZqelPWX5','44mp6LNgEmYEfZMYZQz16ncu7oqcnncGFtz762nC')
+		place = Place()
+		place.name = obj.name
+		place.name = obj.name
+		place.name = obj.name
+		place.name = obj.name
+		place.name = obj.name
+		place.save()
 
 admin.site.register(models.Place,PlaceAdmin)
--- Management/models.py
+++ Management/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
 class Place(models.Model):
-    name = models.CharField(max_length=32, db_index=True)
+    name = models.CharField(max_length=64)
+    photo = models.CharField(max_length=512)
+    latitude = models.FloatField(max_length=32)
+    longitude = models.FloatField(max_length=32)
+    phone = models.CharField(max_length=32)
+    news = models.CharField(max_length=512)
+    open_hours = models.CharField(max_length=128)
+
