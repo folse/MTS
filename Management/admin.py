893ccce4135512c1093e5617877d17725f4735b0
folse <folsee@gmail.com>
2014-08-10 01:04:43 +0800

    Update Models and Admin

--- Management/admin.py
+++ Management/admin.py
@@ -1,26 +1,39 @@
 from django.contrib import admin
 from Management import models
 from parse_rest.connection import register
-from parse_rest.datatypes import Object
+from parse_rest.datatypes import Object, Pointer, Relation,GeoPoint
 
+class Photo(Object):
+	pass
+
+class Category(Object):
+	pass
 
 class Place(Object):
-	pass
+	register('MQRrReTdb9c82PETy0BfUoL0ck6xGpwaZqelPWX5','44mp6LNgEmYEfZMYZQz16ncu7oqcnncGFtz762nC')
+	print 'parse register'
 
 class PlaceAdmin(admin.ModelAdmin):
 
 	def save_model(self, request, obj, form, change):
-		register('MQRrReTdb9c82PETy0BfUoL0ck6xGpwaZqelPWX5','44mp6LNgEmYEfZMYZQz16ncu7oqcnncGFtz762nC')
+		photo = Photo()
+		photo.url = obj.photo
+		photo.save()
+		print photo.objectId
+
+		category = Category()
+		category.name = obj.category
+		category.save()
+
 		place = Place()
 		place.name = obj.name
-		place.photo = obj.photo
 		place.phone = obj.phone
-		place.category = obj.category
-		place.open_hour = obj.open_hour
-		place.latitude = obj.latitude
-		place.longitude = obj.longitude
 		place.news = obj.news
+		place.open_hour = obj.open_hour
 		place.description = obj.description
+		#place.addRelation('photos', 'Place', photo.objectId)
+		place.category = Pointer(category)
+		place.location = GeoPoint(latitude = obj.latitude, longitude = obj.longitude)
 		place.save()
 
 admin.site.register(models.Place,PlaceAdmin)
--- Management/models.py
+++ Management/models.py
@@ -2,13 +2,11 @@ from django.db import models
 
 class Place(models.Model):
     name = models.CharField(max_length=64)
-    photo = models.CharField(max_length=512)
-    phone = models.CharField(max_length=32)
     category = models.CharField(max_length=512)
-    open_hour = models.CharField(max_length=128)
-    latitude = models.FloatField(max_length=32)
-    longitude = models.FloatField(max_length=32)
-    news = models.TextField(max_length=512)
-    description = models.TextField(max_length=512)
-
-
+    photo = models.CharField(max_length=512, blank=True)
+    phone = models.CharField(max_length=32, blank=True)
+    open_hour = models.CharField(max_length=128, blank=True)
+    latitude = models.FloatField(max_length=32, blank=True)
+    longitude = models.FloatField(max_length=32, blank=True)
+    news = models.TextField(max_length=512, blank=True)
+    description = models.TextField(max_length=512, blank=True)
