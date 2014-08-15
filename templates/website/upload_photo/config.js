4dfc312195f1e627a8d706cf21bad0f75b352f2a
folse <folsee@gmail.com>
2014-08-15 12:29:01 +0800

    Add upload_photo

---
+++ templates/website/upload_photo/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'ACCESS_KEY': 'StbRobqxbTkicUShT5AlRXqXs6I7LCEZCk-tmLXz',
+    'SECRET_KEY': 'RuWkSQA1pXyjI_Rn_W4aRylC6cj0q_sgT3m7Xc39',
+    'Bucket_Name': 'ts-image1',
+    'Port': 18080,
+    'Uptoken_Url': '/uptoken',
+    'Domain': 'http://ts-image1.qiniudn.com/'
+};
